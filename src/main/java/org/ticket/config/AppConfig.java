2aa24b909eab51a323fa6a4fcbeb9d95f5686571
detectivedots <mohamed.fareed2001@hotmail.com>
2026-07-15 22:54:18 +0300

    Created AppConfig

---
+++ src/main/java/org/ticket/config/AppConfig.java
@@ -0,0 +1,16 @@
+package org.ticket.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@Configuration
+@ComponentScan(basePackages = "org.ticket")
+@EnableTransactionManagement
+@EnableAspectJAutoProxy
+@PropertySource("classpath:application-${spring.profiles.active}.properties")
+public class AppConfig {
+
+}
